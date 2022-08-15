19db6842dcb1890c324365abeb1c261aa5864ac2
7n14 <hgouge810@gmail.com>
2022-08-15 14:23:55 +0100

    Spindler improvements

--- code/battery/spindler_battery.py
+++ code/battery/spindler_battery.py
@@ -7,7 +7,7 @@ class spindler_battery (battery):
         super(spindler_battery, self).__init__(last_service_date,current_date)
     
     def needs_service(self):#Needs service every 2 years
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 3)
         if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
